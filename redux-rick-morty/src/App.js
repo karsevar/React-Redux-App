265ddcbfae7ad188a6d3b293d5cf253e6ee8ee6e
masonkarsevar <masonkarsevar@gmail.com>
2019-08-15 08:52:55 -0700

    having trouble with react utils

--- redux-rick-morty/src/App.js
+++ redux-rick-morty/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import CharacterList from './components/CharacterList';
+
 function App() {
   return (
     <div className="App">
+      <CharacterList />
     </div>
   );
 }
