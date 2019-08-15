de9dacc97d1c1f3c21403127f679eb53f1814b0d
masonkarsevar <masonkarsevar@gmail.com>
2019-08-15 13:47:10 -0700

    styled components moved to a separate folder and file

--- redux-rick-morty/src/components/CharacterCard.js
+++ redux-rick-morty/src/components/CharacterCard.js
@@ -1,87 +1,18 @@
 
 import React from 'react';
-import styled from 'styled-components';
-
-const CharactersDiv = styled.div`
-  font-family: "sans-serif";
-  margin: 0px 5px 40px 5px;
-  box-shadow: 1px 1px 9px 2px #888888;
-  padding: 10px;
-  border-radius: 10px;
-  width: 320px;
-  
-
-  display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
-`;
-
-const CharacterImg = styled.img`
-  width: 300px;
-  border-radius: 10px;
-`;
-
-const CharacterH3 = styled.h3`
-  font-size: 2.2rem;
-
-  padding: 0;
-  margin-bottom: 5px;
-  margin-top: 5px;
-`;
-
-const CharacterH4 = styled.h3`
-  font-size: 1.2rem;
-
-  padding: 0;
-  margin-bottom: 5px;
-  margin-top: 5px;
-`;
-
-const CharacterParagraph = styled.p`
-  font-size: 1rem;
-
-  padding: 0;
-  margin-bottom: 5px;
-  margin-top: 5px;
-`;
+import {CharacterDiv, CharacterImg, CharacterH3, CharacterH4, CharacterParagraph} from '../styles/appStyles';
 
 const CharacterCard = props => {
     const {character} = props
     return (
-        <CharactersDiv>
+        <CharacterDiv>
             <CharacterImg src={character.image} alt={character.name} />
             <CharacterH3>{character.name}</CharacterH3>
             <CharacterH4>{character.species} - {character.status}</CharacterH4>
             <CharacterParagraph>Location: {character.location.name}</CharacterParagraph>
             <CharacterParagraph>Origin: {character.origin.name}</CharacterParagraph>
-        </CharactersDiv>
+        </CharacterDiv>
     )
 }
 
 export default CharacterCard;
-
-
-// import { Card, Icon, Image } from 'semantic-ui-react'
-
-// const CardExampleCard = () => (
-//   <Card>
-//     <Image src='/images/avatar/large/matthew.png' wrapped ui={false} />
-//     <Card.Content>
-//       <Card.Header>Matthew</Card.Header>
-//       <Card.Meta>
-//         <span className='date'>Joined in 2015</span>
-//       </Card.Meta>
-//       <Card.Description>
-//         Matthew is a musician living in Nashville.
-//       </Card.Description>
-//     </Card.Content>
-//     <Card.Content extra>
-//       <a>
-//         <Icon name='user' />
-//         22 Friends
-//       </a>
-//     </Card.Content>
-//   </Card>
-// )
-
-// export default CardExampleCard
--- redux-rick-morty/src/components/CharacterList.js
+++ redux-rick-morty/src/components/CharacterList.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import styled from 'styled-components';
 import Loader from 'react-loader-spinner';
 
 import CharacterCard from './CharacterCard';
 
 import {getCharacters} from '../actions';
 
-const CharactersDiv = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-evenly;
-`;
-
-const ErrorParagraph = styled.p`
-    padding: 16px;
-    background-color: #ffb7b7;
-    color: red;
-    border: solid 1px #f39595;
-    border-radius: 4px;
-    max-width: 700px;
-    margin: 16px auto;
-`;
+import {CharactersDiv, ErrorParagraph, FetchButton} from '../styles/appStyles';
 
 const CharacterList = props => {
 
@@ -32,11 +17,16 @@ const CharacterList = props => {
 
     return (
         <div className='characters-container'>
-            <button 
-                className='character-button'
-                onClick={fetchCharacters}>
-                {props.isFetching ? 'Loading Data' : 'Fetch Rick and Mortys!'}
-            </button>
+
+            {props.characters.length === 0 ? (
+                <FetchButton
+                    onClick={fetchCharacters}>
+                    {props.isFetching ? 'Loading Data' : 'Fetch Rick and Mortys!'}
+                </FetchButton>
+                ) : (
+                    null
+                )
+            }
 
             {props.isFetching ? (
                 <Loader 
